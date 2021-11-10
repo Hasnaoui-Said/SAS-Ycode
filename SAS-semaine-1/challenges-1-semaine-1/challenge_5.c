669bc1f9b9338f0aa6b1a208907f708b02689a8e
hasnaoui-said <saidhasnaoui.uce@gmail.com>
2021-11-10 17:03:15 +0100

    cooooo

--- SAS-semaine-1/challenges-1-semaine-1/challenge_5.c
+++ SAS-semaine-1/challenges-1-semaine-1/challenge_5.c
@@ -11,11 +11,11 @@ int main(){
 	printf("\n\tIl est");
 	if(C < -20){
 		printf("\tIl est tres froid.");
-	}else if(C > -20 && C < 20){
+	}else if(C > -20 && C < 15){
 		printf("\tIl est froid.");
-	}else if(C < 40){
+	}else if(C > 15 && C < 37){
 		printf("\tIl est chaud.");
-	}else if(C < -20){
+	}else if(C > 37){
 		printf("\tIl est tres chaud.");
 	}
 	return 0;
